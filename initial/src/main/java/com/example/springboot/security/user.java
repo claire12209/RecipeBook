d664fad878d453d459f138f3f0c6d0512bfd1906
cmons <claire.monson@wsu.edu>
2024-10-09 19:51:44 -0700

    error check

---
+++ initial/src/main/java/com/example/springboot/security/user.java
@@ -0,0 +1,12 @@
+package com.example.springboot.security;
+
+public class user {
+    int UserID;
+    String username;
+    String password;
+    String email;
+
+}
+public boolean login(String username, String password){
+
+}
