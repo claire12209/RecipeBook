06804b8a94e899bf2634be64eacf13ee615eba29
Alice Hui <alice.hui@wsu.edu>
2024-10-18 14:02:50 -0700

    dependencies update, application.properties set up, User.java class entity set up

---
+++ complete/src/main/java/com/example/recipebook/User.java
@@ -0,0 +1,55 @@
+package com.example.recipebook;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id;
+
+    @Column(name = "username", nullable = false, length = 50)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "email", nullable = false, length = 255)
+    private String email;
+
+    // Getters and setters
+    public int getUserId() {
+        return user_id;
+    }
+
+    public void setUserId(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
