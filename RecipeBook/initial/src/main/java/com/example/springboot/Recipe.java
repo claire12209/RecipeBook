0cd1dbae124a6cbbe2960b7cbb36c06aeae5053b
alice0791 <alice.hui@wsu.edu>
2024-11-13 19:18:21 -0800

    Imported Recipes, Categories are able to filter, Not displaying recipes yet.

--- RecipeBook/initial/src/main/java/com/example/springboot/Recipe.java
+++ RecipeBook/initial/src/main/java/com/example/springboot/Recipe.java
@@ -1,14 +1,15 @@
 package com.example.springboot;
 
+import java.util.List;
+
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ElementCollection;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.util.List;
 
 @Entity
 @Table(name = "recipes")
